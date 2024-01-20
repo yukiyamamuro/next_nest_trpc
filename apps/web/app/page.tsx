5959db8611187178cd3c94a5c2c42dbdd8072df1
YamamuroY <yuki.sumaho@gmail.com>
2024-01-20 17:02:21 +0900

    chore: add find

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,13 @@ import { trpc } from "@web/app/trpc";
 export default async function Home() {
   const { users } = await trpc.user.list.query();
   const { posts } = await trpc.post.list.query();
+  const { user } = await trpc.user.find.query({ id: "1" });
+
   return (
     <>
       <h1>HOME</h1>
+      YourName {user?.name}
+
       <h2>USER</h2>
       {users.map((user) => (
         <div key={user.id}>
