a7618d3e79d7650860b3c5ea9f0f07a319c05c38
YamamuroY <yuki.sumaho@gmail.com>
2024-02-13 23:22:15 +0900

    chore: add shadcn

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandblue: colors.blue[500],
+        brandred: colors.red[500],
+      },
+    },
+  },
+  variants: {
+    inset: ["even", "odd"],
+  },
+  plugins: [],
+};
